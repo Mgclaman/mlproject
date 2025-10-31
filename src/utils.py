73012300bea8c574bd6a655ab4daf287cd1ba498
Mgclaman <amansinghgurjar810@gmail.com>
2025-10-31 22:53:46 +0530

    Model Training

--- src/utils.py
+++ src/utils.py
@@ -6,8 +6,10 @@ import sys
 import pandas as pd
 import numpy as np
 import dill
+
 from src.exception import CustomException
 from src.logger import logging
+from sklearn.metrics import r2_score
 
 def save_object(file_path: str, obj: object) -> None:
     """Saves a Python object to a file using pickle."""
@@ -18,5 +20,24 @@ def save_object(file_path: str, obj: object) -> None:
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
         logging.info(f"Object saved successfully at {file_path}")
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models) :
+    """Evaluates multiple machine learning models and returns their R2 scores."""
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i] 
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred= model.predict(X_test)
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+            
+        return report
     except Exception as e:
         raise CustomException(e, sys)
